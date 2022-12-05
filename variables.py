e55aafe9c656ad2fe70b8ad66f4252fffdce65b7
youssef <kingyou5599@gmail.com>
2022-12-05 10:41:37 +0100

    ajout de lignes de textes

--- variables.py
+++ variables.py
@@ -1,5 +1,28 @@
+#"l'opérateur d'affectation = permet d'injecter une valeur dans une variable"
+
+# integer, nombre entier
+
+my_number1 = 123
+my_number2 = -42
+
+print(my_number1)
+print(my_number2)
+print(my_number6)
+print(type(my_number6))
+
+# float, nombre à virgule
+
+my_number3 = 3.14
+my_number4 = -2.71
+# 0.0 0. ou .0
+my_number5 = .0
+print(my_number3)
+print(my_number4)
+print(my_number5)
+print(type(my_number3))
+
+
+
 my_text1 = "Ceci est une chaines de caratères"
 my_text2 = 'Ceci est aussi une chaines de caratères'
 
-print(my_text1)
-print(my_text2)
