c9dc071576e67e44695483b8007e3cf27cb0644f
Yeyo9 <116876019+Yeyo9@users.noreply.github.com>
2022-12-13 15:19:22 +0100

    Update exercice-03-06.py

--- exercices-03/exercice-03-06.py
+++ exercices-03/exercice-03-06.py
@@ -9,8 +9,23 @@
 # Affichez un résultat arrondi de la distance en mètres avec la fonction `round()`.
 # Convertissez les mètres en kilo mètres puis stockez le résultat dans la variable `km`.
 # Affichez un résultat arrondi de la distance en kilo mètre avec la fonction `round()`.
-
 miles = 3
 
-# réponse 3.6
+meters = miles * 1609.344
+print(meters)
+réponse = 4828.032
 
+résultat = round(4828.032)
+print(résultat)
+résultat = 4828
+
+# 1 kilomètre = 1000 mètre
+# 0.001 kilomètre = 1 mètre
+
+km = résultat * 0.001
+print(km)
+réponse = 4.828
+
+résultat1 = round(4.828)
+print(résultat1)
+réponse = 5
