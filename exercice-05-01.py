6a12b58375cb4bc71577b1d7aa9a2acd22abf4b4
Yeyo9 <116876019+Yeyo9@users.noreply.github.com>
2022-12-21 10:01:16 +0100

    Update exercice-05-01.py

--- exercice-05-01.py
+++ exercice-05-01.py
@@ -5,5 +5,14 @@
 # réponse 5.1
 
 def multiplication(a: float, b: float) -> float:
+   
+
+    """ Cette fonction permet de multiplier
+    deux nombres
+    a float le premier nombre à multiplier
+    b float le deuxième nombre à multiplier
+    return float le résultat de la multiplication    
+    """
     return a * b
 
+help(multiplication) 
